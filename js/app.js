4191d8c234ff2be5cb846866bac928f4584079b9
Mark Hoffman <mh4913@att.com>
2015-07-19 13:28:34 -0400

    Changed markers array to markerlocation.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-var var_location = {lat: 42.0102, lng: -87.6755};
+var var_location = {lat: 42.0102, lng: -87.6755};  //Center of map on load.
+var tips = []; // An array to hold tips from Foursquare API.
+//var tipSplit = [];
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
@@ -11,7 +13,7 @@ var markers = [
 			name: "rogersPark",
 			latitude: "42.0102",
 			longitude: "-87.6755",
-			venueId: '49bfffd0f964a5203c551fe3',
+			venueId: "49bfffd0f964a5203c551fe3",
 			title:"Rogers Park"
 		},
 		
@@ -19,7 +21,7 @@ var markers = [
 			name: "giardanos",
 			latitude: "42.0065706",
 			longitude: "-87.6614",
-			venueId: '4b9d3afcf964a520969b36e3',
+			venueId: "4b9d3afcf964a520969b36e3",
 			title:"Giardano's Pizza"
 		},
 		
@@ -27,7 +29,7 @@ var markers = [
 			name: "mayne_stage",
 			latitude: "42.008259",
 			longitude: "-87.665028",
-			venueId: '50e61588e4b0e433727bbc2f',
+			venueId: "50e61588e4b0e433727bbc2f",
 			title:"Mayne Stage"
 		},
 		
@@ -35,7 +37,7 @@ var markers = [
 			name: "los_portales",
 			latitude: "42.0080956",
 			longitude: "-87.6667577",
-			venueId: '4af75357f964a5205e0822e3',
+			venueId: "4af75357f964a5205e0822e3",
 			title:"Los Portales"
 		}
 		
@@ -43,45 +45,64 @@ var markers = [
 
 // Search for Foursquare for a user tip for each location; if no tip available
 //  or unable to access Foursquare API, error msg "Listening for latest buzz..." will display.
-function getTips() {
+function getTips(markers) {
 	for (i=0; i < markers.length; i++) {
+		//console.log("getTips " + markers[i].venueId);
+		var tipSplit;
 		var foursquareUrl = 'https://api.foursquare.com/v2/venues/' + 
 		markers[i].venueId + 
 		'?client_id=YRN5IUY2UOMHO3UYB2FKJYRH4AHJVGBP1SVAPPPGGT31FB2E' + 
 		'&client_secret=VHSZ2R1SLQ0I0QB1KZFWU0K2VMBGJIGIDL4P1TYWTDX4OXRM&v=20150504';
-		//console.log(foursquareUrl);
 				$.getJSON(foursquareUrl)
 			.done(function(response){
 				var tipText,
 					tipId,
-					tips;
+					tips,
+					tipSplit;
 				tipId = response.response.venue.id;
-				console.log(tipId);
+				
 				if( response.response.venue.tips.count > 0) {
 					tipText = response.response.venue.tips.groups[0].items[0].text;
 				} else {
 					tipText = "No tips available for this location yet...";
 				}
 				tips = tipId + '%' + tipText;
-				console.log(tips);
+				
 				getTipsCallback(tips);
 			})
 			.fail(function(jqXHR, textStatus, errorThrown) {
 				alert('Error connecting to Foursquare: ' + textStatus);
-			});
-		}
+			});	
+				
+	
+				//console.log("tipSplit = " + tipSplit);	
+	}
+	
+	
 	}
 
 // Update data model (markers array) with tip from Foursquare by matching venue ids
 function getTipsCallback(tip) {
+	
 	var tipSplit = tip.split('%');
 	for (i=0; i<markers.length; i++) {
+		//console.log(markers[i]);
+		console.log("tipSplit[i].tipSplit " + tipSplit[0]);
+		console.log("markers[i].name " + markers[i].name);
+		console.log("markers[i].latitude " + markers[i].latitude);
+		console.log("markers[i].longitude " + markers[i].longitude);
+		console.log("markers[i].venueId " + markers[i].venueId);
+		console.log("markers[i].title " + markers[i].title);
 		if (markers[i].venueId === tipSplit[0]) {
+			console.log("True");
 			markers[i].tips = tipSplit[1];
 			return;
+		} else {
+			console.log("False");
 		}
 	}
-
+	
+	return;
 }
 
 
@@ -102,20 +123,22 @@ function initialize() {
 	 
 	 // Call the ViewModel function and pass in the google.maps.Map object
 	 
-	 getTips();
+	 getTips(markers);
+	 //console.log(markers[1].tips);
 	 setMarkers(markers, map);
 }
            
 
 /*Function that receives attributes from the markers array. */
-var setMarkers = function(markers, map) {
+var setMarkers = function(location, map) {
 	var self = this;
-	
-	self.name = ko.observable(markers.name);
-	self.latitude = ko.observable(markers.latitude);
-	self.longitude = ko.observable(markers.longitude);
-	self.map = ko.observable(markers.map);
-	self.title = ko.observable(markers.title);
+	var markerlocation = [];
+	self.name = ko.observable(location.name);
+	self.latitude = ko.observable(location.latitude);
+	self.longitude = ko.observable(location.longitude);
+	self.map = ko.observable(location.map);
+	self.title = ko.observable(location.title);
+	self.venueId = ko.observable(location.venueId);
 	self.position = ko.computed(function() {
 		return this.latitude() + "," + this.longitude();
 	}, this);
@@ -124,20 +147,26 @@ var setMarkers = function(markers, map) {
 	 * markers model.  The setMap() function places the markers on the map
 	 * as defined by the latitude and longitude properties.  
 	 * */
-	for (i=0; i<markers.length; i++) {
-		
-		markers[i] = new google.maps.Marker({
-			position: new google.maps.LatLng(markers[i].latitude,
-			markers[i].longitude), 
-			title: markers[i].title   
+	for (i=0; i<location.length; i++) {
+		//console.log(location[i].title);
+		//console.log(location[i].latitude);
+		//console.log(location[i].venueId);
+		markerlocation[i] = new google.maps.Marker({
+			position: new google.maps.LatLng(location[i].latitude,
+			location[i].longitude), 
+			title: location[i].title,  
+			tips: location[i].venueId
    	});
-	markers[i].setMap(map);
 	
 	// added this line.
+	//console.log(markers[i].tips);
 	var infowindow = new google.maps.InfoWindow({
+			content: "Hello there, " + location[i].venueId,
 			maxWidth: 200});
-			
-	infowindow.open(map, markers[i]);
+	
+	
+	markerlocation[i].setMap(map);		
+	infowindow.open(map, markerlocation[i]);
 
 	};
 };
@@ -145,14 +174,10 @@ var setMarkers = function(markers, map) {
 
 var myViewModel = function(myMap) {
 	var self = this;
-	//console.log(self);
 	this.markerList = ko.observableArray([]);
-	//this.marker = ko.observableArray([]);
 	
 	markers.forEach(function(markerItem){
 		self.markerList.push(new setMarkers(markerItem) );
-		//self.markerList
-		//self.markerList.setMap(myMap);
 	});
 	
 	return self.markerList;
