d8ce70b9fbeb51d1096152060a1413a0c548bf1e
Mark Hoffman <mh4913@att.com>
2015-07-25 10:09:02 -0400

    Finally got the InfoWindow to work.  Still working on Nav bar.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
+var map;
 var var_location = {lat: 42.0102, lng: -87.6755};  //Center of map on load.
+var streetViewImage;
+var streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=180x90&location=';
 
 google.maps.event.addDomListener(window, 'load', initialize);// Load the map on load and call the initialize function.
 
@@ -11,6 +14,9 @@ var markers = [
 			name: "rogersParkSocial",
 			latitude: "42.0102",
 			longitude: "-87.6755",
+			streetAddress: "6922 N. Glenwood Ave",
+			cityAddress:  "Chicago, Illinois, 60626",
+			url: "www.rogersparksocial.com",
 			venueId: "536300da498ee44e63dcbc1d",
 			tips: "Waiting for Foursquare tips...",
 			title:"Rogers Park Social"
@@ -20,6 +26,9 @@ var markers = [
 			name: "giordanos",
 			latitude: "42.0065706",
 			longitude: "-87.6614",
+			streetAddress: "6836 North Sheridan Road",
+			cityAddress:  "Chicago, Illinois, 60626",
+			url: "www.giordanos.com",
 			venueId: "4a189b92f964a520d0791fe3",
 			tips: "Waiting for Foursquare tips...",
 			title:"Giordano's Pizza"
@@ -29,6 +38,9 @@ var markers = [
 			name: "mayne_stage",
 			latitude: "42.008259",
 			longitude: "-87.665028",
+			streetAddress: "1328 West Morse Avenue",
+			cityAddress:  "Chicago, Illinois, 60626",
+			url: "www.maynestage.com",
 			venueId: "4b7acc0cf964a520403d2fe3",
 			tips: "Waiting for Foursquare tips...",
 			title:"Mayne Stage"
@@ -38,6 +50,9 @@ var markers = [
 			name: "los_portales",
 			latitude: "42.0080956",
 			longitude: "-87.6667577",
+			streetAddress: "1418 W Morse Ave",
+			cityAddress:  "Chicago, Illinois, 60626",
+			url: "www.losportalesmexicanrestaurantchicago.com",
 			venueId: "4a91eac3f964a520551c20e3",
 			tips: "Waiting for Foursquare tips...",
 			title:"Los Portales"
@@ -96,70 +111,89 @@ function splitTips(tip) {
 function initialize() {
 	var mapOptions = {
 		center: new google.maps.LatLng(var_location.lat, var_location.lng), 
-		zoom: 16
+		zoom: 16,
+        mapTypeControl: false,
+        disableDefaultUI: true
 		};	
-	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    if($(window).width() <= 1080) {
+        mapOptions.zoom = 13;
+    }
+    if ($(window).width() < 850 || $(window).height() < 595) {
+        hideNav();
+    }
+		
+	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	
 	 // Call the ViewModel function and pass in the google.maps.Map object
 	 connectFourSquare();
-	 setMarkers(markers, map);
-}
+	 setMarkers(markers);
+};
            
 /*Function that receives attributes from the markers array. */
-var setMarkers = function(location, map) {
-	//var self = this;
-	//var marker = [];
-	var myPosition = [];
-	var contentString = [];
-	/* Temporary ko.observables to check the marker object attributes outside of map */
-	this.name = ko.observable(location.name);
-	this.latitude = ko.observable(location.latitude);
-	this.longitude = ko.observable(location.longitude);
-	this.map = ko.observable(location.map);
-	this.title = ko.observable(location.title);
-	this.venueId = ko.observable(location.venueId);
-	this.tips = ko.observable(location.tips);
-	this.position = ko.computed(function() {
-		return this.latitude() + "," + this.longitude();
-	}, this);
+ var setMarkers = function(location) {
+	var position = [];
 	
-	//infowindow = new google.maps.InfoWindow();
 	/*This for loop will initialize each of the markers as defined in the 
-	 * markers model.  The setMap() function places the markers on the map
+	 * markers model and places the markers on the map
 	 * as defined by the latitude and longitude properties.  
 	 * */
 	for (i=0; i<location.length; i++) {
-		myPosition[i] = new google.maps.LatLng(location[i].latitude,location[i].longitude);
-		marker = new google.maps.Marker({
-			position: myPosition[i],
+		position[i] = new google.maps.LatLng(location[i].latitude,location[i].longitude);
+		
+		location[i].holdMarker = new google.maps.Marker({
+			position: position[i],
+			map: map,
 			title: location[i].title,  
 			venueId: location[i].venueId,
 			tips: location[i].tips
    	});
    	
-	marker.setMap(map);	
+   	
+        //Binds infoWindow content to each marker
+     location[i].contentString = '<img src="' + streetViewUrl + location[i].streetAddress + location[i].cityAddress +
+             '" alt="Street View Image of ' + location[i].title + '"><br><hr style="margin-bottom: 5px"><strong>' + 
+             location[i].title + '</strong><br><p>' + 
+             location[i].streetAddress + '<br>' + 
+             location[i].cityAddress + '<br></p><a class="web-links" href="http://' + location[i].url + 
+             '" target="_blank">' + location[i].url + '</a>';
+   	 // location[i].contentString = "Welcome to " + location[i].title + "</br>" + location[i].tips;
+   	   	
+  var infowindow = new google.maps.InfoWindow({
+            content: location[i].contentString
+        });
+            
+   new google.maps.event.addListener(location[i].holdMarker, 'click', (function(marker, i) {
+      return function() {
+      infowindow.setContent(location[i].contentString);
+      infowindow.open(map,this);
+      var windowWidth = $(window).width();
+         if(windowWidth <= 1080) {
+            map.setZoom(14);
+         } else if(windowWidth > 1080) {
+            map.setZoom(16);  
+         }
+         map.setCenter(marker.getPosition());
+         location[i].picBoolTest = true;
+          }; 
+        })(location[i].holdMarker, i));
+        
+        
+        var searchNav = $('#nav' + i);
+        searchNav.click((function(marker, i) {
+          return function() {
+            infowindow.setContent(location[i].contentString);
+            infowindow.open(map,marker);
+            map.setZoom(20);
+            map.setCenter(marker.getPosition());
+            //location[i].picBoolTest = true;
+          }; 
+        })(location[i].holdMarker, i));
+    
+  };
+   	
+};	
 	
-	//contentString[i] = location[i].title + '<br>' + location[i].tips + '<br>' + 
-	//location[i].latitude + ' ' + location[i].longitude;
-	
-			//console.log("Content String" + contentString[i]);
-	
-	 infowindow = new google.maps.InfoWindow({
-			content: location[i].title + "<br>" + location[i].tips + "<br>" + 
-			location[i].latitude + " " + location[i].longitude,
-			//position: myPosition[i],
-			maxWidth: 200
-			});	
-	
-	google.maps.event.addListener(marker, 'click', function() {
-    //infowindow.setContent(this.contentString[i]);
-	infowindow.open(map, this);
- });
-		console.log("MyPosition " + myPosition[i]);
-		console.log("markerlocation " + marker.title);
-	};	
-};
-
-/* The ViewModel */
+ //The ViewModel 
 var myViewModel = function(myMap) {
 	var self = this;
 	this.markerList = ko.observableArray([]);
